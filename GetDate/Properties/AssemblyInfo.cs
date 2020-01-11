03201f7c29ea36b0d166bce9c407e3ef74e9fb6e
David Elfleet <djesoft@hotmail.com>
2020-01-11 22:07:48 +0000

    Add project files.

---
+++ GetDate/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetDate")]
+[assembly: AssemblyDescription("Calculates number of minutes since 01 Jan 2000 for a given date and time")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DJESoft")]
+[assembly: AssemblyProduct("GetDate")]
+[assembly: AssemblyCopyright("Copyright © DJE 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5cc8d46-6f6e-41c3-8f71-25302efa12f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
